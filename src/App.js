6766b63c8099559b0ce442ffdb38c86d665f7f7e
헹구 <hyemin085@gmail.com>
2021-11-02 13:52:46 +0900

    [추가] 2021.11.02 기본 틀 작업

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.scss';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+//페이지
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Route path="/" exact component={Home} />
+      </Router>
     </div>
+    </>
   );
 }
 
